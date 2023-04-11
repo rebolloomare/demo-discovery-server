5d3d35db5c4fe7c99d246bd1a878d215463f0c74
omar.rebollo <omar.rebollo@gmail.com>
2023-04-10 23:44:21 -0600

    Initial commit

---
+++ src/main/java/omare/com/mx/discoveryserver/DemoDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package omare.com.mx.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/omare/com/mx/discoveryserver/DemoDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package omare.com.mx.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
